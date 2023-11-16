098558fde616aa5eedd2264633183b4633c8969b
guiweb85@gmail.com <guiweb85@gmail.com>
2023-11-16 20:04:46 -0300

    17. Criando URL para acesso da imagem para renderização

--- src/main/java/io/github/guilhermendes/imageliteapi/domain/service/ImageService.java
+++ src/main/java/io/github/guilhermendes/imageliteapi/domain/service/ImageService.java
@@ -2,6 +2,10 @@ package io.github.guilhermendes.imageliteapi.domain.service;
 
 import io.github.guilhermendes.imageliteapi.domain.entity.Image;
 
+import java.util.Optional;
+
 public interface ImageService {
     Image save(Image image);
+
+    Optional<Image> getById(String id);
 }
