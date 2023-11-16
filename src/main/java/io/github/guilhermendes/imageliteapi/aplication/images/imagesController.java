cc1150d392cc366e9fc1a0c1ec941501a81e0542
guiweb85@gmail.com <guiweb85@gmail.com>
2023-11-16 19:24:17 -0300

    16. Criar a URL da imagem e configurando o tamanho do Upload

--- src/main/java/io/github/guilhermendes/imageliteapi/aplication/images/imagesController.java
+++ src/main/java/io/github/guilhermendes/imageliteapi/aplication/images/imagesController.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 
@@ -24,6 +26,7 @@ import java.util.List;
 public class imagesController {
 
     private final ImageService service;
+    private final ImagesMapper mapper;
 
     @PostMapping
     public ResponseEntity save(
@@ -33,15 +36,19 @@ public class imagesController {
         ) throws IOException {
         log.info("Imagem recebida: name:{}, size:{}, file{}", file.getOriginalFilename(), file.getSize() );
 
-        Image image = Image.builder()
-                .name(name)
-                .tags(String.join(",", tags))
-                .size(file.getSize())
-                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
-                .file(file.getBytes())
-                .build();
+        Image image = mapper.mapToImage(file, name, tags);
+        Image savedImage = service.save(image);
+        URI imagemUri = buildImageURL(savedImage);
 
-        service.save(image);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.created(imagemUri).build();
+    }
+
+    private URI buildImageURL(Image image){
+        String imagePath = "/" + image.getId();
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path(imagePath)
+                .build()
+                .toUri();
     }
 }
