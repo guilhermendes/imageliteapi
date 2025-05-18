86b506e6b2b1df74760e8fa8a98a122b20e01bf7
Guilherme Guimaraes <guiweb85@gmail.com>
2025-05-18 20:07:16 -0300

    12. Mapeamento JPA para a entidade Image

--- src/main/java/io/github/guilhermendes/imageliteapi/ImageliteapiApplication.java
+++ src/main/java/io/github/guilhermendes/imageliteapi/ImageliteapiApplication.java
@@ -1,9 +1,19 @@
 package io.github.guilhermendes.imageliteapi;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ImageliteapiApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/io/github/guilhermendes/imageliteapi/domain/enums/ImageExtension.java
@@ -0,0 +1,8 @@
+package io.github.guilhermendes.imageliteapi.domain.enums;
+
+public enum ImageExtension {
+    PNG,
+    JPG,
+    GIF,
+    JPEG
+}
