853142e67d06d86db368da03ec418654d8b307a7
Guilherme Guimaraes <guiweb85@gmail.com>
2025-05-26 20:24:21 -0300

    16. Criar a URL da imagem e configurando o tamanho do Upload

---
+++ src/main/java/io/github/guilhermendes/imageliteapi/application/images/ImagensMapper.java
@@ -0,0 +1,24 @@
+package io.github.guilhermendes.imageliteapi.application.images;
+
+import io.github.guilhermendes.imageliteapi.domain.entity.Image;
+import io.github.guilhermendes.imageliteapi.domain.enums.ImageExtension;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class ImagensMapper {
+
+    public Image mapToImage(MultipartFile file, String name, List<String> tags) throws IOException {
+        return Image.builder()
+                .name(name)
+                .tags(String.join(",", tags))
+                .size(file.getSize())
+                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
+                .file(file.getBytes())
+                .build();
+    }
+}
--- src/main/java/io/github/guilhermendes/imageliteapi/application/images/ImagesController.java
+++ src/main/java/io/github/guilhermendes/imageliteapi/application/images/ImagesController.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,6 +26,7 @@ import java.util.List;
 public class ImagesController {
 
     private final ImageService service;
+    private final ImagensMapper mapper;
 
     @PostMapping
     public ResponseEntity save(
@@ -33,17 +36,18 @@ public class ImagesController {
     ) throws IOException {
     log.info("Imagem recebida: name{}, size: {}", file.getOriginalFilename(), file.getSize());
 
+    Image image = mapper.mapToImage(file, name, tags);
+    Image savedImage = service.save(image);
+    URI imageUri = buildImageURL(savedImage);
 
-    Image image = Image.builder()
-        .name(name)
-        .tags(String.join(",", tags))
-        .size(file.getSize())
-        .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
-            .file(file.getBytes())
-        .build();
+    return ResponseEntity.created(imageUri).build();
+    }
 
-    service.save(image);
-
-    return ResponseEntity.ok().build();
+    private URI buildImageURL(Image image){
+        String imagePath = "/" + image.getId();
+        return ServletUriComponentsBuilder.
+                fromCurrentRequest()
+                .path(imagePath)
+                .build().toUri();
     }
 }
